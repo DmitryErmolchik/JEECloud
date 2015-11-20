f3b68d56797d3979f0eb0e8d46d53f1c2a90f038
DmitryErmolchik <queer82@gmail.com>
2015-11-20 12:55:11 +0300

    Produces were added

--- core/core-bl/src/main/java/com/dim4tech/jeecloud/core/service/configurator/PropertiesConfiguratorServiceImpl.java
+++ core/core-bl/src/main/java/com/dim4tech/jeecloud/core/service/configurator/PropertiesConfiguratorServiceImpl.java
@@ -1,32 +1,40 @@
 package com.dim4tech.jeecloud.core.service.configurator;
 
+import com.dim4tech.jeecloud.core.annotation.ConfigurationProperty;
 import com.dim4tech.jeecloud.core.domain.exception.JEECloudException;
 import com.dim4tech.jeecloud.core.domain.exception.JEECloudExceptionMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.Startup;
 import javax.ejb.Stateless;
-import java.io.File;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Properties;
 
+@Startup
 @Stateless
 public class PropertiesConfiguratorServiceImpl implements ConfiguratorService {
 
     private final static Logger LOG = LoggerFactory.getLogger(PropertiesConfiguratorServiceImpl.class);
     private Properties properties = new Properties();
 
+    @Inject
+    @ConfigurationProperty
+    private Instance<String> propertiesLocationInstance;
+
     @PostConstruct
     private void loadProperties() {
-        try {
-            properties.load(this.getClass().getResourceAsStream("/application.properties"));
-        } catch (IOException e) {
-            LOG.error(e.getMessage());
-            throw new JEECloudException(new JEECloudExceptionMessage(e.getMessage()));
-        }
+            propertiesLocationInstance.forEach(value -> {
+                try {
+                    properties.load(this.getClass().getResourceAsStream(value));
+                } catch (IOException e) {
+                    LOG.error(e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            });
     }
 
     @Override
---
+++ servicelibrary/servicelibrary-bl/src/main/java/com/dim4tech/jeecloud/library/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package com.dim4tech.jeecloud.library;
+
+import com.dim4tech.jeecloud.core.annotation.ConfigurationProperty;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+
+@Dependent
+public class ApplicationConfiguration {
+    @Produces
+    @ConfigurationProperty
+    private String applicationConfiguraton = "/application.properties";
+}
