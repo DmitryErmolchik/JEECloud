6077ab43734efbf95534d119f4075f3c1cc7ce8d
DmitryErmolchik <queer82@gmail.com>
2015-11-20 16:01:29 +0300

    Error message localized

---
+++ core/core-api/src/main/java/com/dim4tech/jeecloud/core/annotation/LocalizationConfiguration.java
@@ -0,0 +1,19 @@
+package com.dim4tech.jeecloud.core.annotation;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
+public @interface LocalizationConfiguration {
+    @Nonbinding
+    String path() default "";
+
+    @Nonbinding
+    String bundle() default "";
+}
